b998099ae74163e7188e5251dc0407399a7e7716
Daniel Silva <daniel@Daniels-MacBook-Air.local>
2022-02-10 14:44:16 -0600

    maybe working?

--- server/server.js
+++ server/server.js
@@ -8,5 +8,10 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('pubic'))
 
-// app.get('/', (req, res) => 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.js'))
+}) 
     
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server running on ${port}`))
