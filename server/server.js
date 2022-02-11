c3b99bcd1cf011e8cd23114373d04cf0f6d9c58a
Daniel Silva <daniel@Daniels-MacBook-Air.local>
2022-02-10 20:13:35 -0600

    test

--- server/server.js
+++ server/server.js
@@ -20,7 +20,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 }) 
 
-app.get('/rollbar-test', test)
+app.get('/rollbar-test', (req, res) => {
+    try{
+        test()
+    } catch(error) {
+        rollbar.warning(error)
+    }
+})
 
 
 
